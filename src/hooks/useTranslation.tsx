e32749bc3dcc41297164c8983a15be9f26de00b1
alikemalcelenk <alikemalcelenkk@gmail.com>
2024-06-29 21:57:40 +0300

    update use translation hook fpr single case

--- src/hooks/useTranslation.tsx
+++ src/hooks/useTranslation.tsx
@@ -3,7 +3,8 @@ import {useTranslation as useTranslationI18} from 'react-i18next';
 const useTranslation = (page: string, section?: string) => {
   const {t} = useTranslationI18(page);
 
-  const tCustom = (sectionInner: string) => t(`${section}.${sectionInner}`);
+  const tCustom = (sectionInner: string) =>
+    t(section ? `${section}.${sectionInner}` : `${sectionInner}`);
 
   return {t: tCustom};
 };
