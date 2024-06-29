24f8b985984b2e9e3d2d9f58785cd36e57686c63
Seçkin <seckin_brke@hotmail.com>
2024-06-29 17:44:50 +0300

    Ios changed package name

--- ios/gammaaiTests/aiGammaChatTests.m
+++ ios/gammaaiTests/aiGammaChatTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface aiGammaChatTests : XCTestCase
+@interface gammaaiTests : XCTestCase
 
 @end
 
-@implementation aiGammaChatTests
+@implementation gammaaiTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
